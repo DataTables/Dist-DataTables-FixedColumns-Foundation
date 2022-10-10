9bc9d9e9e27f4647d0049fa821a5e68242478160
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:12 +0000

    Add support for mjs files (ES Modules)

---
+++ js/fixedColumns.foundation.mjs
@@ -0,0 +1,13 @@
+
+/*! Foundation integration for DataTables' FixedColumns
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-zf';
+import DataTable from 'datatables.net-fixedcolumns';
+
+
+
+
+export default DataTable;
