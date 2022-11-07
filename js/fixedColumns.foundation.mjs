8a0bd9e9291a75a5f1f0b4aa501e7bc2d4bd83a9
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:12:40 +0000

    7bc6dcd5053b2d6128798325bbdf202dc14ef38c Release 4.2.1 (packaging error with 4.2.0)
    Sync to source repo @7bc6dcd5053b2d6128798325bbdf202dc14ef38c

--- js/fixedColumns.foundation.mjs
+++ js/fixedColumns.foundation.mjs
@@ -1,11 +1,10 @@
-
 /*! Foundation integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-zf';
-import DataTable from 'datatables.net-fixedcolumns';
+import 'datatables.net-fixedcolumns';
 
 
 
